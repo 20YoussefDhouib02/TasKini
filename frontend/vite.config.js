bba195def6157043e6124d49f4b0f63534f73d0d
20YoussefDhouib02 <youssefdhouib2014@gmail.com>
2024-12-25 18:07:35 +0100

    Last update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Load environment variables
+const API_BASE_URL = process.env.VITE_API_BASE_URL || "http://localhost:8800";
+
 export default defineConfig({
   plugins: [react()],
 
+  // Local development server configuration
   server: {
-    port: 3000,
+    port: 3000, // Only affects local development
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
-        changeOrigin: true,
+        target: API_BASE_URL, // Proxy API requests to the backend
+        changeOrigin: true, // Ensures the host header matches the target
       },
     },
   },
--- server/index.js
+++ server/index.js
@@ -15,9 +15,17 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://taskini.vercel.app"],
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Origin Not allowed"));
+      }
+    },
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
